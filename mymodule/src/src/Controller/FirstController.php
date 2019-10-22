657c018dfe3ccffd97ad861f5a00f9957ea419b7
rajju <vajrachara@yahoo.com>
2019-10-22 14:34:54 +0100

    module_form_alter implementation

---
+++ mymodule/src/src/Controller/FirstController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contain \Drupal\mymodule\Controller\MyModuleController.
+ */
+
+namespace Drupal\mymodule\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class FirstController extends ControllerBase{
+  public function content(){
+    return array(
+      '#type' => 'markup',
+      'markup' => t('This is menu link custom page')
+    );
+  }
+}
