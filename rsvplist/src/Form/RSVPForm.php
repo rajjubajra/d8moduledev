f4eb5a4a0d209a7f58635b5dc36a151280346859
rajju <vajrachara@yahoo.com>
2019-10-23 15:17:09 +0100

    RSVP list module implementation

--- rsvplist/src/Form/RSVPForm.php
+++ rsvplist/src/Form/RSVPForm.php
@@ -53,7 +53,7 @@ class RSVPForm extends FormBase{
    */
   public function validateForm(array &$form, FormStateInterface $form_state){
     $value = $form_state->getValue('email');
-    if($value == !\Drupal::service(email.validator)->is_valid($value)){
+    if($value == !\Drupal::service('email.validator')->isValid($value)){
       $form_state->setErrorByName('email', t('The email address %mail is not valid', array('%mail'=>$value)));
     }
   }
