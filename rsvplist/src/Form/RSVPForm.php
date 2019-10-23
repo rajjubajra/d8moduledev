3d9c89172fdbcac129271291d963ed5ac456fa63
rajju <vajrachara@yahoo.com>
2019-10-23 08:32:43 +0100

    RSVP list module implementation

--- rsvplist/src/Form/RSVPForm.php
+++ rsvplist/src/Form/RSVPForm.php
@@ -27,7 +27,7 @@ class RSVPForm extends FormBase{
    * (@inheritdoc)
    */
   public function buildForm(array $form, FormStateInterface $form_state){
-    $node = \Drupal::routeMatch()->getParamter('node');
+    $node = \Drupal::routeMatch()->getParameter('node');
     $nid = $node->nid->value;
     $form['email'] = array(
       '#title'=> t('Email Address'),
