cfd6580a92bf2d9f4e65007e5aad6c012ab94ddb
rajju <vajrachara@yahoo.com>
2019-10-24 07:06:55 +0100

    RSVP list module implementation

--- rsvplist/src/Form/RSVPForm.php
+++ rsvplist/src/Form/RSVPForm.php
@@ -48,15 +48,7 @@ class RSVPForm extends FormBase{
     );
     return $form;
   }
-  /**
-   * (@inheritdoc)
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state){
-    $value = $form_state->getValue('email');
-    if($value == !\Drupal::service('email.validator')->isValid($value)){
-      $form_state->setErrorByName('email', t('The email address %mail is not valid', array('%mail'=>$value)));
-    }
-  }
+  
   /**
     * (@inheritdoc)
     */
