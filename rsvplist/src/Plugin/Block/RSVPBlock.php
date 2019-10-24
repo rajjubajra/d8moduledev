4591259053dc77d62ba0a13aedfde1eacd9cad59
rajju <vajrachara@yahoo.com>
2019-10-24 16:38:12 +0100

    RSVP list module implementation

---
+++ rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Plugin\Block\RSVPBlock
+ */
+
+namespace Drupal\rsvplist\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides and RSVP list block
+ * @Block(
+ *  id = "rsvp_block"
+ *  admin_label = @Translation("RSVP Block"),
+ * )
+ */
+
+class RSVPBlock extends BlockBase{
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    return array(
+      '#markup' => $this->t('RSVP List Block'),
+    );
+  }
+}
